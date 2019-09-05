f6a0719249f065435836ebcc526992296a26c8ec
TorrieL <hmminicity2@163.com>
2019-09-05 22:07:57 +0800

    differences between arrow function and traditional way to name a function-this

--- D3&Data/index.js
+++ D3&Data/index.js
@@ -6,8 +6,18 @@ const svg = d3.select('svg');
 
 const rect = svg.select('rect')
   .data(data)
-  .attr('width',200)
-  .attr('height',100)
-  .attr('fill','pink')
+  .attr('width',(d,i,n)=>{
+    console.log('arrow function this: ',this)
+    console.log(i);
+    console.log(n);
+    return d.width
+  })
+  .attr('height',function(d){
+    console.log('traditional function this: ', this)
+    return d.height
+  })
+  .attr('fill',function(d){
+    return d.fill
+  })
 
   console.log(rect)
