742429be9e99f37fa0726cce775fb504176f2095
TorrieL <hmminicity2@163.com>
2019-09-05 22:20:51 +0800

    show rect group

--- D3&Data/index.js
+++ D3&Data/index.js
@@ -1,13 +1,15 @@
 const data = [
-  {width:200, height: 100, fill: 'purple'}
+  {width:200, height: 100, fill: 'purple'},
+  {width:100, height: 60, fill: 'pink'},
+  {width:50, height: 30, fill: 'khaki'},
 ];
 
 const svg = d3.select('svg');
 
-const rect = svg.select('rect')
+const rect = svg.selectAll('rect')
   .data(data)
   .attr('width',(d,i,n)=>{
-    console.log('arrow function this: ',this)
+    //console.log('arrow function this: ',this)
     console.log(n[i]);
     return d.width
   })
