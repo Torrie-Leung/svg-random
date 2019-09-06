aa41c63c68c14b3337420cc5d547771bd9ecd943
TorrieL <hmminicity2@163.com>
2019-09-07 01:10:42 +0800

    Creating a Linear Scale

--- D3&Data/workWithJSON/index.js
+++ D3&Data/workWithJSON/index.js
@@ -2,7 +2,7 @@
 const svg = d3.select('svg');
 
 d3.json('planets.json').then(data => {
-  console.log(data)
+  //console.log(data)
   const circs = svg.selectAll('circle')
     .data(data);
 
@@ -12,7 +12,7 @@ d3.json('planets.json').then(data => {
       .attr('r',d => d.radius)
       .attr('fill',d => d.fill);
 
-     console.log(circs) 
+     //console.log(circs) 
     //append the enter selection to the DOM
     circs.enter()
       .append('circle')
@@ -20,4 +20,30 @@ d3.json('planets.json').then(data => {
       .attr('cx',d => d.distance)
       .attr('r',d => d.radius)
       .attr('fill',d => d.fill);
+})
+
+const line = d3.select('#line');
+//console.log(line)
+d3.json('menu.json').then(data => {
+
+  const y = d3.scaleLinear()
+    .domain([0,1000])
+    .range([0,500]);
+  // console.log(y(0))
+  // console.log(y(999))
+  // console.log(y(1004))
+  const charts = line.selectAll('rect')
+    .data(data)
+  //console.log(charts)
+  charts.attr('width',50)
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x',(d,i) => i*70)
+
+  charts.enter()
+    .append('rect')
+      .attr('width',50)
+      .attr('height', d => y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x',(d,i) => i*70)
 })
