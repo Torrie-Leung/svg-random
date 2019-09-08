c431653839c9b0ed2cc5209bb80bc8b084f03ecb
TorrieL <hmminicity2@163.com>
2019-09-08 11:21:23 +0800

    inverting scales

--- D3&Data/workWithJSON/barChart.js
+++ D3&Data/workWithJSON/barChart.js
@@ -5,7 +5,7 @@ const svg = d3.select('.canvas')
     .attr('height',605);
 
 //create margins and dimensions
-const margin = {top:10,right:10,bottom:15,left:25};
+const margin = {top:10,right:10,bottom:15,left:30};
 const graphWidth = 600 - margin.left -margin.right;
 const graphHeight = 600 - margin.top -margin.bottom;
 
@@ -24,11 +24,11 @@ d3.json('menu.json').then(data => {
 
   const y = d3.scaleLinear()
     .domain([0,max])
-    .range([0,graphHeight]);
+    .range([graphHeight,0]);
 
   const x = d3.scaleBand()
     .domain(data.map(item => item.name))
-    .range([0,600])
+    .range([0,500])
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
@@ -36,17 +36,20 @@ d3.json('menu.json').then(data => {
     .data(data)
   //console.log(rects)
   rects.attr('width',x.bandwidth)
-    .attr('height', d => y(d.orders))
+    .attr('height', d => graphHeight - y(d.orders))
     .attr('fill', 'orange')
     .attr('x',(d,i) => x(d.name))
-
+    .attr('y',(d,i) => y(d.orders))
+  //console.log(graphHeight-y(200))
+  
 //append the enter selection to the DOM
   rects.enter()
     .append('rect')
       .attr('width',x.bandwidth)
-      .attr('height', d => y(d.orders))
+      .attr('height', d =>graphHeight - y(d.orders))
       .attr('fill', 'orange')
       .attr('x',(d,i) => x(d.name))
+      .attr('y',(d,i) => y(d.orders))
 
 //create and call the axes
   const xAxis = d3.axisBottom(x);
