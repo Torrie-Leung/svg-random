49c9c439bab09e5428b70f48760bcabb9bc483f0
TorrieL <hmminicity2@163.com>
2019-09-15 18:37:09 +0800

    create a update function

--- D3&Data/workWithJSON/barChart.js
+++ D3&Data/workWithJSON/barChart.js
@@ -18,6 +18,63 @@ const xAxisGroup = graph.append('g')
   .attr('transform',`translate(0,${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
+//scales(which not depend on the data)
+const y = d3.scaleLinear()
+  .range([graphHeight,0]);
+
+const x = d3.scaleBand()
+  .range([0,500])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
+//create the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+  .ticks(7)
+  .tickFormat(tickNum => tickNum + ' orders');
+
+//update x axis text
+xAxisGroup.selectAll('text')
+  .attr('transform','rotate(-45)')
+  .attr('text-anchor','end')
+  .attr('fill','orange')
+
+//update function
+const update = (data) => {
+
+  //updating scale domain
+  y.domain([0,d3.max(data, d => d.orders)])
+
+  x.domain(data.map(item => item.name))
+
+  //join the data to rects
+  const rects = graph.selectAll('rect')
+    .data(data)
+
+  //remove exit selection
+  rects.exit().remove()
+
+  //update current shapes in dom
+  rects.attr('width',x.bandwidth)
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x',(d,i) => x(d.name))
+    .attr('y',(d,i) => y(d.orders))
+
+  //append the enter selection to the DOM
+  rects.enter()
+    .append('rect')
+      .attr('width',x.bandwidth)
+      .attr('height', d =>graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x',(d,i) => x(d.name))
+      .attr('y',(d,i) => y(d.orders))
+  //call the axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+}
+
+//get data from firestore
 db.collection('dishes').get().then(res => {
   const data = [];
   res.docs.forEach(doc => {
@@ -27,46 +84,17 @@ db.collection('dishes').get().then(res => {
   const min = d3.min(data, d => d.orders);
   const max = d3.max(data, d => d.orders);
 
-  const y = d3.scaleLinear()
-    .domain([0,max])
-    .range([graphHeight,0]);
-
-  const x = d3.scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0,500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
-
-  const rects = graph.selectAll('rect')
-    .data(data)
+  
+  
   //console.log(rects)
-  rects.attr('width',x.bandwidth)
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x',(d,i) => x(d.name))
-    .attr('y',(d,i) => y(d.orders))
+  
   //console.log(graphHeight-y(200))
 
-//append the enter selection to the DOM
-  rects.enter()
-    .append('rect')
-      .attr('width',x.bandwidth)
-      .attr('height', d =>graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x',(d,i) => x(d.name))
-      .attr('y',(d,i) => y(d.orders))
+  update(data)
 
-//create and call the axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y)
-    .ticks(7)
-    .tickFormat(tickNum => tickNum + ' orders');
+  
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
+  
 
-  xAxisGroup.selectAll('text')
-    .attr('transform','rotate(-45)')
-    .attr('text-anchor','end')
-    .attr('fill','orange')
+  
 })
