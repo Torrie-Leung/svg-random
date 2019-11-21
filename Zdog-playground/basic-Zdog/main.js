7c77113da255097b6c9b9abb39085865f263830f
TorrieL <hmminicity2@163.com>
2019-11-21 21:13:11 +0800

    cube copy

--- Zdog-playground/basic-Zdog/main.js
+++ Zdog-playground/basic-Zdog/main.js
@@ -2,22 +2,46 @@ const cubes = new Zdog.Illustration({
   element: '.cubes',
   resize: 'fullscreen',
   dragRotate: true,
+  zoom:2
 })
 
 const cubeOne = new Zdog.Box({
   addTo: cubes,
-  width:200,
-  height:200,
-  depth:200,
+  width:60,
+  height:60,
+  depth:60,
   stroke:false,
   leftFace: '#da0',
   rightFace: '#de5',
-  topFace:'#ad3',
+  topFace:'#ed0',
   bottomFace:'#76d'
 })
+
+const cubeTwo = new Zdog.Box({
+  addTo: cubes,
+  width:30,
+  height:30,
+  depth:30,
+  stroke:false,
+  leftFace: '#da0',
+  rightFace: '#de5',
+  topFace:'#ed0',
+  bottomFace:'#76d',
+  translate: {
+    z:100
+  }
+})
+const cubeThree = cubeTwo.copy({
+  translate: {
+    z:-100
+  }
+})
+
 function animateCubes(){
-  cubes.rotate.y += 0.001;
-  cubes.rotate.x += 0.002;
+  cubes.rotate.y += 0.011;
+  cubes.rotate.x += 0.012;
+  cubeTwo.rotate.z += 0.01
+  cubeThree.rotate.z += 0.11
   cubes.updateRenderGraph()
   requestAnimationFrame(animateCubes)
 }
