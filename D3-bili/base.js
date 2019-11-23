c17499d2f88ede64da17611d01312102b585a7e4
TorrieL <hmminicity2@163.com>
2019-11-23 23:48:48 +0800

    encapsulation&callback update

--- D3-bili/base.js
+++ D3-bili/base.js
@@ -1,5 +1,6 @@
 //create a dataset
 const dataset = [30,70,33,44,55,11,100,89,99];
+const dataset1 = [60,170,233,44,55,111,100,19,99];
 //create svg
 const width = 300;
 const height = 300;
@@ -11,20 +12,57 @@ const svg = d3.select("#svg")
               .attr("width",width)
               .attr("height",height);
 
-svg.selectAll("rect").data(dataset).enter()
-    .append("rect")
-    .attr("fill","#daa520")
+const genRect = obj => {
+  obj.attr("fill","#daa520")
     .attr("x",(data,index) => padding.left+index*rectStep)
     .attr("y",(data,index) => height-padding.bottom-data)
     .attr("width",rectWidth)
     .attr("height",data=>data);
+}
+// genRect(svg.selectAll("rect").data(dataset).enter().append("rect"))
 
-const text = svg.selectAll("text").data(dataset).enter().append("text")
-                .attr("fill","#fff")
-                .attr("font-size","14px")
-                .attr("text-anchor","middle")
-                .attr("x",(data,index)=>padding.left+index*rectStep)
-                .attr("y",(data,index) => height-padding.bottom-data)
-                .text(data=>data)
-                .attr("dx",rectWidth/2)
-                .attr("dy","1em");
+//create rects
+// svg.selectAll("rect").data(dataset).enter()
+//     .append("rect")
+//     .attr("fill","#daa520")
+//     .attr("x",(data,index) => padding.left+index*rectStep)
+//     .attr("y",(data,index) => height-padding.bottom-data)
+//     .attr("width",rectWidth)
+//     .attr("height",data=>data);
+
+const genText = obj => {
+  obj.attr("fill","#fff")
+      .attr("font-size","12px")
+      .attr("text-anchor","middle")
+      .attr("x",(data,index)=>padding.left+index*rectStep)
+      .attr("y",(data,index) => height-padding.bottom-data)
+      .text(data=>data)
+      .attr("dx",rectWidth/2)
+      .attr("dy","1em");
+}
+// genText(svg.selectAll("text").data(dataset).enter().append("text"))
+//create texts
+// const text = svg.selectAll("text").data(dataset).enter().append("text")
+//                 .attr("fill","#fff")
+//                 .attr("font-size","12px")
+//                 .attr("text-anchor","middle")
+//                 .attr("x",(data,index)=>padding.left+index*rectStep)
+//                 .attr("y",(data,index) => height-padding.bottom-data)
+//                 .text(data=>data)
+//                 .attr("dx",rectWidth/2)
+//                 .attr("dy","1em");
+
+const init = (dataset,callback) =>{
+  genRect(svg.selectAll("rect").data(dataset).enter().append("rect"));
+  genText(svg.selectAll("text").data(dataset).enter().append("text"));
+  setTimeout(()=>{
+    update(dataset1)
+  },3000)
+};
+
+const update = dataset =>{
+  genRect(svg.selectAll("rect").data(dataset));
+  genText(svg.selectAll("text").data(dataset));
+};
+
+init(dataset);
