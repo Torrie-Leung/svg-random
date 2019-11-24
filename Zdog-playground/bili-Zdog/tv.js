d9413a918078c77c8c96c3d8ad15f9cc9fe8edf6
TorrieL <hmminicity2@163.com>
2019-11-24 17:51:42 +0800

    tv anime

--- Zdog-playground/bili-Zdog/tv.js
+++ Zdog-playground/bili-Zdog/tv.js
@@ -4,7 +4,14 @@ const tv = new Zdog.Illustration({
   dragRotate: true,
 });
 
+let ticker = 0;
+let cycleCount = 250;
 function animation(){
+  let progress = ticker / cycleCount;
+  // apply easing to rotation
+  let tween = Zdog.easeInOut( progress % 1, 3 );
+  tv.rotate.y = tween*Zdog.TAU;
+  ticker++;
   tv.updateRenderGraph();
   requestAnimationFrame(animation);
 }
@@ -32,4 +39,68 @@ const roundRectF = new Zdog.RoundedRect({
 
 const roundRectB = roundRectF.copy({
   translate:{z:-28}
+})
+
+//eyes
+let eye_1 = new Zdog.Shape({
+  addTo:tv,
+  path: [
+    { x: -8 },//start at 1st point
+    { x: 8 },//line to 2nd point
+  ],
+  translate:{
+    z:28,
+    x:-15,
+    y:-10
+  },
+  rotate: { z:-(Zdog.TAU/12)},
+  stroke:2,
+  color:'#000'
+});
+
+let eye_2 = eye_1.copy({
+  translate:{
+    z:28,
+    x:15,
+    y:-10
+  },
+  rotate: { z:(Zdog.TAU/12)}
+})
+
+//mouth
+let mouth = eye_1.copy({
+  translate:{
+    z:28,
+    y:10
+  },
+  rotate: { z:0}
+})
+
+//athena
+eye_1.copy({
+  path:[
+    { x :-10 },
+    { x: 10}
+  ],
+  translate:{
+    z:-5,
+    x:-15,
+    y:-40
+  },
+  stroke:10,
+  rotate: { z:(Zdog.TAU/9)}
+})
+
+eye_1.copy({
+  path:[
+    { x :-10 },
+    { x: 10}
+  ],
+  translate:{
+    z:-5,
+    x:15,
+    y:-40
+  },
+  stroke:10,
+  rotate: { z:-(Zdog.TAU/9)}
 })
