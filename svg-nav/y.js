b51f58773c2e4e61ed798f30e458731d09d6ea33
TorrieL <hmminicity2@163.com>
2019-11-30 23:21:03 +0800

    make a x

--- svg-nav/y.js
+++ svg-nav/y.js
@@ -7,10 +7,20 @@ window.onload = function(){
   const tlm = new TimelineMax({});
   const toggleMenu = new TimelineMax({paused:true,reversed:true})
   // TweenMax.to(icon,2,{x:100});
-  icon.addEventListener('mouseenter', () => {
-    // tlm
-    //   .to(lineOne,0.5,{scaleX: 1.5})
-    //   .to(lineOne,0.5,{scaleX: 1})
-    tlm.staggerTo(lines,0.25,{scaleX:1.5,repeat:5,yoyo:true},0.175)
+  // icon.addEventListener('mouseenter', () => {
+  //   // tlm
+  //   //   .to(lineOne,0.5,{scaleX: 1.5})
+  //   //   .to(lineOne,0.5,{scaleX: 1})
+  //   tlm.staggerTo(lines,0.25,{scaleX:1.5,repeat:5,yoyo:true},0.175)
+  // })
+
+  toggleMenu
+    .to(lineTwo,.25,{scaleX:0})
+    .to(lineOne,.25,{rotation:45,transformOrigin:"50% 50%",y:8})
+    .to(lineThree,.25,{rotation:-45,x:8,y:8})
+
+  icon.addEventListener('click', _ =>{
+    console.log('clicked')
+    toggleMenu.reversed() ? toggleMenu.play() :toggleMenu.reverse()
   })
 };
