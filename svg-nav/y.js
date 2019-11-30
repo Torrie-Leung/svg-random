81811d2fba9d218aed11a44f98a7c79e82d038b9
TorrieL <hmminicity2@163.com>
2019-11-30 23:07:08 +0800

    tlm

--- svg-nav/y.js
+++ svg-nav/y.js
@@ -1,4 +1,16 @@
 window.onload = function(){
   const icon = document.querySelector('.icon')
-  TweenMax.to(icon,2,{x:100});
+  const lineOne = icon.querySelector('.line-one')
+  const lineTwo = icon.querySelector('.line-two')
+  const lineThree = icon.querySelector('.line-three')
+  const lines = [lineOne,lineTwo,lineThree]
+  const tlm = new TimelineMax({});
+  const toggleMenu = new TimelineMax({paused:true,reversed:true})
+  // TweenMax.to(icon,2,{x:100});
+  icon.addEventListener('mouseenter', () => {
+    // tlm
+    //   .to(lineOne,0.5,{scaleX: 1.5})
+    //   .to(lineOne,0.5,{scaleX: 1})
+    tlm.staggerTo(lines,0.25,{scaleX:1.5,repeat:5,yoyo:true},0.175)
+  })
 };
