12be1704fd9c37868cf6e81c3f97c97a7bffee0b
TorrieL <hmminicity2@163.com>
2020-02-15 13:49:58 +0800

    set multiple arg to stroke-dasharray

--- svg-basic/storke-path/app.js
+++ svg-basic/storke-path/app.js
@@ -1,8 +1,19 @@
 window.onload = function(){
   function SVGRoad(ele){
     this.pathLength = ele.getTotalLength()
-    this.setStrokeDasharrayInPercent = function(strokeDasharray){
-      ele.style.strokeDasharray = (strokeDasharray/100) * this.pathLength
+    // single arg
+    // this.setStrokeDasharrayInPercent = function(strokeDasharray){
+    //   ele.style.strokeDasharray = (strokeDasharray/100) * this.pathLength
+    // }
+
+    //multipul args
+    this.setStrokeDasharrayInPercent = function(){
+      console.log(arguments)
+      let mstrokeDasharray = ""
+      for(i = 0; i< arguments.length; i++){
+        mstrokeDasharray += (arguments[i]/100) * this.pathLength + ' '
+      }
+      ele.style.strokeDasharray = mstrokeDasharray
     }
 
     this.setStrokeDashoffsetInPercent = function(strokeDashoffset){
@@ -13,6 +24,6 @@ window.onload = function(){
   const pathEle = document.getElementById('path');
   const SVGRoadInstance = new SVGRoad(pathEle);
   console.log(SVGRoadInstance.pathLength)
-  SVGRoadInstance.setStrokeDasharrayInPercent(30)
-  SVGRoadInstance.setStrokeDashoffsetInPercent(10)
+  SVGRoadInstance.setStrokeDasharrayInPercent(30,15)
+  SVGRoadInstance.setStrokeDashoffsetInPercent(3)
 }
